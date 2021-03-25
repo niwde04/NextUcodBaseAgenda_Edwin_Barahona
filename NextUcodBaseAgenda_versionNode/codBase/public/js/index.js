cf5fef04d6ca3414b71835cfae591e66b8530701
Edwin Barahona <ed_barah@hotmail.com>
2021-03-25 14:51:02 -0600

    Login
    Login completo

--- NextUcodBaseAgenda_versionNode/codBase/public/js/index.js
+++ NextUcodBaseAgenda_versionNode/codBase/public/js/index.js
@@ -1,5 +1,40 @@
+function validarUsuario() {
+    var Usuario = $('#user');
+    var Password = $('#pass');
+
+    if (Usuario.val() != "" && Password.val() != "") {
+
+        $.ajax({
+             url: '/login',
+             method: 'POST',
+             contentType: "application/json",
+             dataType: "json",
+             data: JSON.stringify({
+                 usuario: Usuario.val(),
+                 password: Password.val()
+             }),
+             success: function(res) {
+
+                 let authorize = (JSON.stringify(res.success))
+                 
+                 if (authorize) {
+                     window.location.href = "http://localhost:3000/main.html";
+                 }
+             }
+         }) 
 
 
-$("#buscar").click(function(){
-    console.log("hola")
+    } else {
+        alert("Complete todos los campos");
+    }
+}
+
+$( document ).ready(function() {
+    alert("Usuario: edwin, Password: 1234");
+});
+
+
+$("#login").click(function () {
+    console.log('click')
+    validarUsuario()
 })
--- NextUcodBaseAgenda_versionNode/codBase/server/index.js
+++ NextUcodBaseAgenda_versionNode/codBase/server/index.js
@@ -1,16 +1,70 @@
-var http        = require('http'),
-    express     = require('express')
+require('./connetion.js'); // importa el archivo de conexión
+const User = require('../model/user.js'); // importa el esquema
+const 	express = require('express'),
+		app = express(),
+        port = process.env.PORT || 3000
 
+        app.use (express.json());
+app.get('/crearUsuario',function(req,res){
 
-var port    = port = process.env.PORT || 3000,
-    app     = express(),
-    Server  = http.createServer(app)
+    User.find({}, (err, usuarios) => {
+        if (err){
+            return res.status(500).json({
+                success: false,
+                message:("Error al intentar obtener los usuarios. (status:500)")
+            })
+        } else{
+            if (usuarios.length <= 0) {
+                const user = new User({ usuario: 'edwin', password:'1234' }); // crea la entidad
+                user.save(); // guarda en bd
+            } else{
+                res.status(200).json({
+                    success: true,
+                    message:"si se pudo",
+                    payload: usuarios
+                })
+            }
 
-    app.use('/',express.static('../public'))
-
-
-    app.listen(port, function(){
-        console.log ("server is running on port: http://localhost:"+port)
-
+        }
     })
+})
+
+app.post('/login',function(req,res){
+
+    let username = req.body.usuario
+    let password = req.body.password
+
+    User.find({usuario:username, password: password}, function(err,userFind){
+        if (err) {
+            return res.status(500).send({message: 'Error al intentar obtener el usuario en el inicio de sesión. (status:500)'})
+          }else{
+              console.log(userFind)
+            if(userFind.length == 1){
+
+                res.status(200).json({
+                    success: true,
+                    message: "Login correcto"
+                })
+            }else{
+                res.status(401).json({
+                    success: false,
+                    message: "Login incorrecto"
+                })
+
+            }
+          }
+    })
+
+  
+
+})
+
+
+app.use('/', express.static('../public'));
+app.listen(port, function() {
+    console.log('Servidor corriendo en http://localhost:'+port)
+})
+
+
+
 
